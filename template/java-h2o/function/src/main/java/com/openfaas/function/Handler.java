1227ae777d2f1d2060bd1b5fa0f225f937e47f1d
dermatologist <github@gulfdoctor.net>
2019-11-04 20:23:24 -0500

    update Handle

--- template/java-h2o/function/src/main/java/com/openfaas/function/Handler.java
+++ template/java-h2o/function/src/main/java/com/openfaas/function/Handler.java
@@ -5,10 +5,13 @@ import com.openfaas.model.IResponse;
 import com.openfaas.model.IRequest;
 import com.openfaas.model.Response;
 
-import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.parser.IParser;
-import org.hl7.fhir.dstu3.model.*;
-import java.util.List;
+import hex.genmodel.easy.RowData;
+import hex.genmodel.easy.EasyPredictModelWrapper;
+import hex.genmodel.easy.prediction.*;
+import hex.genmodel.MojoModel;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 // Your function imported from GitHub
 import in.co.dermatologist.YourClass;
@@ -17,67 +20,66 @@ public class Handler implements com.openfaas.model.IHandler {
 
     public IResponse Handle(IRequest req) {
         Response res = new Response();
-        FhirContext ctx = FhirContext.forDstu3();
-        IParser parser = ctx.newJsonParser();
+        Gson gson = new Gson();
 
-        Patient patient = new Patient();
-        Observation observation = new Observation();
-        Subscription subscription = new Subscription();
-        CarePlan carePlan = new CarePlan();        
         try{
 
             // Request Bundle
-            Bundle bundle_in = parser.parseResource(Bundle.class,req.getBody());
-            List<Bundle.BundleEntryComponent> components = bundle_in.getEntry();
+            // Bundle bundle_in = parser.parseResource(Bundle.class,req.getBody());
+            Received received = gson.fromJson(req.getBody(), Received.class);
+            String model = System.getenv("MODEL_PATH") + "model.zip";
 
-            // Process CarePlan
-            for (Bundle.BundleEntryComponent component: components){
-                Resource resource = component.getResource();
-                if(resource.getResourceType() == ResourceType.Patient){
-                    patient = (Patient)resource;
-                }
-                if(resource.getResourceType() == ResourceType.Observation){
-                    observation = (Observation)resource;
-                }
-                if(resource.getResourceType() == ResourceType.Subscription){
-                    subscription = (Subscription)resource;
-                }
-                if(resource.getResourceType() == ResourceType.CarePlan){
-                    carePlan = (CarePlan)resource;
-                    carePlan.setId("NewId");
-                }
+            EasyPredictModelWrapper.Config config = new EasyPredictModelWrapper.Config()
+                    //.setModel(MojoModel.load("/home/bell/Documents/work/temp/2019-11-04_13:55:10.zip"))
+                    .setModel(MojoModel.load(model))
+                    .setEnableContributions(true);
+            //.setEnableLeafAssignment(true);
+            EasyPredictModelWrapper model = new EasyPredictModelWrapper(config);
+
+            RowData row = new RowData();
+            Iterator payloadIterator = received.payload();
+            while(payloadIterator.hasNext()) {
+                row.put(iterator.next(), iterator.next());
             }
+            BinomialModelPrediction p = model.predictBinomial(row);
+            received.setPrediction(p.label);
+            for (int i = 0; i < p.classProbabilities.length; i++) {
+                received.setProbability(p.classProbabilities[i]);
+            }
+//            ArrayList<String> list = new ArrayList<String>();
 
-            // Response Bundle
-            Bundle bundle = new Bundle();
-            bundle.setType(Bundle.BundleType.MESSAGE);
 
-            Bundle.BundleEntryComponent bundleEntryComponent = new Bundle.BundleEntryComponent();
-            bundleEntryComponent.setResource(patient);
-            bundle.addEntry(bundleEntryComponent);
-
-            Bundle.BundleEntryComponent bundleEntryComponent1 = new Bundle.BundleEntryComponent();
-            bundleEntryComponent1.setResource(observation);
-            bundle.addEntry(bundleEntryComponent1);
-
-            Bundle.BundleEntryComponent bundleEntryComponent2 = new Bundle.BundleEntryComponent();
-            bundleEntryComponent2.setResource(subscription);
-            bundle.addEntry(bundleEntryComponent2);
-
-            Bundle.BundleEntryComponent bundleEntryComponent3 = new Bundle.BundleEntryComponent();
-            bundleEntryComponent3.setResource(carePlan);
-            bundle.addEntry(bundleEntryComponent3);
-
-            res.setBody(parser.encodeResourceToString(bundle));
         }
     	catch(Exception e){
-            Endpoint endpoint = new Endpoint();
-            endpoint.setId("MyId");
-
-            res.setBody(parser.encodeResourceToString(endpoint));        
+ 
+            // res.setBody(parser.encodeResourceToString(endpoint));        
     	}
 
 	    return res;
     }
 }
 
+// Request is mapped into an instance of this class
+// Create a class for response like this.
+// This template uses the same class for both.
+class Received {
+    private String Prediction;
+    private ArrayList<long> Probability = new ArrayList<long>();
+
+    private ArrayList<String> Payload;
+
+    Received (){}
+
+    public int prediction() { return this.Prediction; }
+    public ArrayList<String> payload() { return this.Payload; }
+    public void setPayload(ArrayList<String> payload){
+        this.Payload = payload;
+    }
+    public void setPrediction(String prediction){
+        this.Prediction = prediction;
+    }
+    public void setProbability(long probability){
+        this.Probability.add(probability);
+    }
+
+}
